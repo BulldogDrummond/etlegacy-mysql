02675b985d025620257f287d4d51dc0f5eff8cc9
Bulldog Drummond <butchdrummond@gmail.com>
2013-02-02 09:35:12 -0500

    server: Add schema for a very basic test table.

---
+++ misc/etlegacy-dump.sql
@@ -0,0 +1,51 @@
+-- Host: localhost    Database: etlegacy
+-- ------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `server_status`
+--
+
+DROP TABLE IF EXISTS `server_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `server_status` (
+  `ss_uid` mediumint(8) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Unique Row ID',
+  `ss_key` varchar(20) NOT NULL COMMENT 'Status Key',
+  `ss_val` varchar(40) NOT NULL COMMENT 'Status Value',
+  `ss_persist` smallint(6) NOT NULL DEFAULT '0' COMMENT 'Set to 1 to retain in database',
+  PRIMARY KEY (`ss_uid`),
+  UNIQUE KEY `ss_key` (`ss_key`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Server Status';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `server_status`
+--
+
+LOCK TABLES `server_status` WRITE;
+/*!40000 ALTER TABLE `server_status` DISABLE KEYS */;
+INSERT INTO `server_status` VALUES (1,'DB_Test','OK',1);
+/*!40000 ALTER TABLE `server_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
